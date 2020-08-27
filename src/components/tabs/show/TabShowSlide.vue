000cc9c16c596a0342f2d610bdfd85a1c5f60f32
zdYng <qianzhongdao@163.com>
2020-08-27 17:58:45 +0800

    first commit

---
+++ src/components/tabs/show/TabShowSlide.vue
@@ -0,0 +1,39 @@
+<template>
+    <div class="tab-standard-show">
+        <el-carousel indicator-position="outside">
+            <el-carousel-item v-for="item in 4" :key="item">
+                <h3>{{ item }}</h3>
+            </el-carousel-item>
+        </el-carousel>
+    </div>
+
+</template>
+
+<style>
+    .el-carousel__item h3 {
+        color: #475669;
+        font-size: 18px;
+        opacity: 0.75;
+        line-height: 300px;
+        margin: 0;
+    }
+
+    .el-carousel__item:nth-child(2n) {
+        background-color: #99a9bf;
+    }
+
+    .el-carousel__item:nth-child(2n+1) {
+        background-color: #d3dce6;
+    }
+</style>
+
+<style lang="less" scoped>
+
+   /deep/ .tab-standard-show{
+       width:100%;
+       height: 100%;
+       margin: 5px;
+       padding: 5px;
+}
+</style>
+
